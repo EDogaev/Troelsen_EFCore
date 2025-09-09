a62d39b139d79d7164732c5bb774957ffcd8b992
EDogaev <edogaev@yandex.ru>
2025-09-09 11:54:00 +0300

    Создание модели Car

--- AutoLot.Samples/ApplicationDbContext.cs
+++ AutoLot.Samples/ApplicationDbContext.cs
@@ -1,18 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoLot.Samples.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoLot.Samples
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Car> Cars { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Обращения к Fluent API.
-           // OnModelCreatingPartial(modelBuilder);
+            // OnModelCreatingPartial(modelBuilder);
         }
 
         //partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
--- AutoLot.Samples/Program.cs
+++ AutoLot.Samples/Program.cs
@@ -1,9 +1,2 @@
-﻿using AutoLot.Samples;
-
-var cntxt = new ApplicationDbContextFactory().CreateDbContext(new []{""});
-//Console.WriteLine(cntxt.Database.);
-
-
-
-Console.ReadLine();
+﻿
 
