b0d58fb84886f6d1845e192c4d4724ea8a4384c0
EDogaev <edogaev@yandex.ru>
2025-09-16 11:06:24 +0300

    Добавление фабрики контекста данных

---
+++ AutoLot.Dal/EfStructures/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AutoLot.Dal.EfStructures;
+
+public class ApplicationDbContextFactory: IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        const string connectionString = "Server=.,5433;DataBase=AutoLot;Password=Vac_2310;User ID=sa;TrustServerCertificate=true";
+        optionsBuilder.UseSqlServer(connectionString);
+        Console.WriteLine(connectionString);
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
